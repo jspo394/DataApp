fabe9bf87573731c9ca0c3c20516d93076aa1d3d
jspo394 <jhoansebastianpadillaortiz@gmail.com>
2022-11-28 20:19:15 -0500

    28 de noviembre
    ultimos cambios, sitio final se agrega información de los creadores

--- app.R
+++ app.R
@@ -8,34 +8,34 @@ library(writexl)
 library(stringr)
 library(ggplot2)
 
-ui<-dashboardPage(title= "DatApp", skin= "blue",
-                  dashboardHeader(title="DatApp",
-                                  dropdownMenu(type="messages",
-                                               messageItem(from="Abner",
-                                                           "Hola")
-                                               
-                                  )
+#titulo del sitio
+ui<-dashboardPage(title= "DatApp",
+                  dashboardHeader(title="DatApp",tags$li(actionLink("openModal", label = "",
+                                                                    icon = icon("address-book")),class = "dropdown")
                                   
                   ),
                   
                   dashboardSidebar(
                     
-                    
+                    #menu
                     sidebarMenu(id="sidebarID",
+                                
+                                menuItem("Archivo a trabajar",icon = icon("database"),href="https://drive.google.com/file/d/11osYZHTN0Z6JK6uXgEh9JDv-aW-_8wk4/view?usp=share_link"),
                                 menuItem("Cargar documento",icon = icon("upload"),
                                          fileInput("file", "Csv o Excel", multiple = TRUE,accept = c(".csv", ".xlsx"))),
                                 
                                 menuItem("Descargar",icon = icon("download"),downloadButton("downloadData")),
-                                menuItem("Tableu",icon = icon("dashboard"),href = "https://public.tableau.com/app/profile/javier.mauricio.bedoya.gonzalez/viz/Anlisis_Brecha_Digital_Colombia/Hoja1"),
-                                menuItem("Contenido",id = "chartsID",icon = icon("rss"),
-                                         menuSubItem("Blog"),
-                                         menuSubItem("Quienes somos")
-                                         
-                                )
+                                menuItem("Tableu",icon = icon("dashboard"),href = "https://public.tableau.com/app/profile/javier.mauricio.bedoya.gonzalez/viz/Anlisis_Brecha_Digital_Colombia/Hoja1")#,
+                                # menuItem("Contenido",id = "chartsID",icon = icon("rss"),
+                                #          menuSubItem("Blog"),
+                                #          menuSubItem("Quienes somos")
+                                #          
+                                # )
                     )
                     
                     
                   ),
+                  #muestra la tabla del contenido de la base de datos
                   dashboardBody(
                     DT::dataTableOutput("contents"),plotOutput("fig")
                     
@@ -45,6 +45,18 @@ ui<-dashboardPage(title= "DatApp", skin= "blue",
 
 server <- function(input, output){
   
+  
+  #muestra el icono con información de los creadores
+  observeEvent(input$openModal, {
+    showModal(
+      modalDialog(title = "Autores Dashboard",
+                  p("Contacto:"),
+                  p("Diana Marcela Velez Posso","-dmvelezp@unadvirtual.edu.co"),
+                  p("Jhoan Sebastian Padilla Ortiz","-jspadillao@unadvirtual.edu.co"),
+      )
+    )
+  })
+  #limpia los datos
   text1 <- reactive({
     
     req(input$file)
@@ -59,18 +71,23 @@ server <- function(input, output){
       datafile <- read.csv(input$file$datapath, header = TRUE, sep = ",")
     }
   })
-  
+  #muestra la tabla
   output$contents <- DT::renderDataTable({
     DT::datatable(text1())
   })
-  
+  #muestra la grafica
   output$fig <- renderPlot(
-    ggplot(data = text1(), aes(Nombre))+geom_bar()
+    ggplot(data = text1(), aes(x=Cargo_identificado))+ geom_bar(width=0.2,colour="blue") + 
+      ggtitle("Podemos observar que el cargo que más aparece es el de Desarrollador,
+      esta base de datos hace parte del Estudio de identificación de brechas de
+      capital humano para el sector TIC y se usó como insumo para la construcción
+      del tablero de resultados del estudio: Tablero Desempeño Actual del Talento Humano")+ coord_flip()
   )
+  #sirve para descargar el archivo trabajado
   output$downloadData <- downloadHandler(
-    filename = "prueba.xlsx",
+    filename = "brechasTic.csv",
     content = function(file) {
-      write_xlsx(text1(), file)
+    write_csv(text1(), file)
     }
   )
   
